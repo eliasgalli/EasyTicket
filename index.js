2b1aab72de515078444aac6881af64d61a6a5618
elias <eliasgalli@gmail.com>
2021-06-15 09:04:38 -0300

    versión inicial

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path')
+const morgan = require('morgan');
+const mysql = require('mysql');
+const mconn = require('express-myconnection');
+
+const app = express();
+
+const ClienteRoute = require('./routes/cliente')
+
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.use(morgan('dev'));
+app.use(mconn(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port:3306,
+    database:'tickets'
+},'single'))
+
+app.use('/',ClienteRoute);
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.listen(app.get('port'), function() {
+  console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
